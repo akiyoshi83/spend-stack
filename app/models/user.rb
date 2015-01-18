c9693fd92fa7a2d96e9d9d1a498dcb5b089256d1
akiyoshi83 <mail@akiyoshi.info>
2015-01-18 14:43:04 +0900

    implement minimal authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,22 @@
 class User < ActiveRecord::Base
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider  = auth[:provider]
+      user.uid       = auth[:uid]
+      user.nickname  = auth[:info][:nickname]
+      user.name      = auth[:info][:name]
+      user.image_url = auth[:info][:image]
+    end
+  end
+
+  def self.find_by_omniauth(auth)
+    provider  = auth[:provider]
+    uid       = auth[:uid]
+    User.find_by(provider: provider, uid: uid)
+  end
+
+  def self.find_or_create_by_omniauth(auth)
+    User.find_by_omniauth(auth) ||
+      User.create_with_omniauth(auth)
+  end
 end
