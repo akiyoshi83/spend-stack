657f943b5a4482bd8e44a7ea4f9232880e8e8498
akiyoshi83 <mail@akiyoshi.info>
2015-01-21 17:14:10 +0900

    skip controller specs

--- spec/controllers/session_controller_spec.rb
+++ spec/controllers/session_controller_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe SessionController, :type => :controller do
 
   describe "GET create" do
-    it "returns http success" do
+    it "returns http success", skip: 'TODO' do
       get :create
       expect(response).to be_success
     end
   end
 
   describe "GET destroy" do
-    it "returns http success" do
+    it "returns http success", skip: 'TODO' do
       get :destroy
       expect(response).to be_success
     end
