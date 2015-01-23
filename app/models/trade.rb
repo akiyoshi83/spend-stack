bfaa40465e9183c5089b7667356b46a412f2777c
akiyoshi83 <mail@akiyoshi.info>
2015-01-23 15:29:25 +0900

    add model specs

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -3,4 +3,11 @@ class Trade < ActiveRecord::Base
   belongs_to :category
 
   acts_as_taggable
+
+  validates :amount,
+    presence: true,
+    numericality: { only_integer: true }
+  validates :trade_at, presence: true
+  validates :memo, length: { maximum: 256 }
+  validates :user, presence: true
 end
