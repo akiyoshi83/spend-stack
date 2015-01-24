ab1334b2ab8bdec5cb61a4bb64d947c061cae317
akiyoshi83 <mail@akiyoshi.info>
2015-01-24 15:34:58 +0900

    revert loose error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  rescue_from Exception, :with => :error_render
+  #rescue_from Exception, :with => :error_render
 
   helper_method :current_user
 
