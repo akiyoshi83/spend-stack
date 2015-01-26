90bd62c42ccf54a83d5479152e196a1d79021067
akiyoshi83 <mail@akiyoshi.info>
2015-01-26 17:41:58 +0900

    revert again 23a3a63
    specを一時的にrevertして戻すのを忘れていた。

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,13 @@ class User < ActiveRecord::Base
 
   acts_as_tagger
 
+  validates :provider, presence: true
+  validates :uid, presence: true
+  validate  :uniqueness_user
+  validates :image_url, format: {
+    with: /(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix, allow_blank: true
+  }
+
   def self.create_with_omniauth(auth)
     create! do |user|
       user.provider  = auth[:provider]
@@ -23,5 +30,12 @@ class User < ActiveRecord::Base
   def self.find_or_create_by_omniauth(auth)
     User.find_by_omniauth(auth) ||
       User.create_with_omniauth(auth)
+  end
+
+  def uniqueness_user
+    one = User.find_by(provider: provider, uid: uid)
+    if one && one != self
+      errors.add(:uniqueness_user, "has already existed #{provider} #{uid}")
+    end
   end
 end
